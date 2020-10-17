921d4ae56d5441e3a8f207050646c59c474176a4
Anders Børjesson <anbo@zealand.dk>
2020-10-17 14:17:55 +0200

    Add project files.

---
+++ TodoApiUsingDatabase/Controllers/TodoItems2Controller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TodoApiUsingDatabase.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApiUsingDatabase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItems2Controller : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoItems2Controller(TodoContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<TodoItems2Controller>
+        [HttpGet]
+        public IEnumerable<TodoItem> Get()
+        {
+            return _context.TodoItems;
+        }
+
+        // GET api/<TodoItems2Controller>/5
+        [HttpGet("{id}")]
+        public TodoItem Get(int id)
+        {
+            return _context.TodoItems.FirstOrDefault(item => item.Id == id);
+        }
+
+        // POST api/<TodoItems2Controller>
+        [HttpPost]
+        public TodoItem Post([FromBody] TodoItem value)
+        {
+            // TODO generated Id??
+            _context.TodoItems.Add(value);
+            _context.SaveChangesAsync();
+            return value;
+        }
+
+        /*/ PUT api/<TodoItems2Controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] TodoItem value)
+        {
+        
+        }*/
+
+        // DELETE api/<TodoItems2Controller>/5
+        [HttpDelete("{id}")]
+        public TodoItem Delete(int id)
+        {
+            TodoItem item = _context.TodoItems.FirstOrDefault(todoItem => todoItem.Id == id);
+            if (item == null) return null;
+            _context.TodoItems.Remove(item);
+            return item;
+        }
+    }
+}
---
+++ TodoApiUsingDatabase/Startup.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using TodoApiUsingDatabase.Models;
+
+namespace TodoApiUsingDatabase
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+//            services.AddDbContext<TodoContext>(opt =>
+//                opt.UseInMemoryDatabase("TodoList"));
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseSqlServer("Server=tcp:anbo-databaseserver.database.windows.net,1433;Initial Catalog=anbobase;Persist Security Info=False;User ID=anbo;Password=Hemmelig14;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
